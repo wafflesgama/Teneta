666b65863529f18aceb2bfe6044d34339c957ded
Guilherme Gama <guilherme.s.gam@gmail.com>
2022-01-23 16:31:46 +0000

    Project Bulk commit

---
+++ Assets/OpenCV+Unity/Demo/LiveSketch_WebCam/LiveSketchScript.cs
@@ -0,0 +1,130 @@
+﻿namespace OpenCvSharp.Demo
+{
+    using UnityEngine;
+    using OpenCvSharp;
+    using System.Threading.Tasks;
+
+    public class LiveSketchScript : WebCamera
+    {
+        public int blurSize = 5;
+        public float thres = 70f;
+        public float maxVal = 255f;
+        public int mode = 0;
+        public double canny1 = 10;
+        public double canny2 = 70;
+        public bool modeC;
+        public double analFactor;
+        int counter = 0;
+        Mat refImage;
+        Mat img;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            this.forceFrontalCamera = true;
+        }
+
+
+        private async void AddRef()
+        {
+            await Task.Delay(3000);
+            refImage = img;
+        }
+        // Our sketch generation function
+        protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)
+        {
+
+            img = Unity.TextureToMat(input, TextureParameters);
+            counter++;
+            //Convert image to grayscale
+            Mat imgGray = new Mat();
+            Cv2.CvtColor(img, imgGray, ColorConversionCodes.BGR2GRAY);
+
+
+            Mat imgMed = new Mat();
+            Cv2.MedianBlur(img, imgMed, blurSize);
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                AddRef();
+                return true;
+            }
+
+
+            //if (Input.GetKey(KeyCode.T))
+            //{
+            //    anal = imgGray;
+            //}
+
+            //if (anal != null)
+            //{
+            //    //analFactor = Cv2.(anal, imgGray, ShapeMatchModes.I1);
+            //}
+
+
+
+            if (refImage == null) return true;
+
+            // Clean up image using Gaussian Blur
+            //Mat imgGrayBlur = new Mat();
+            //Cv2.GaussianBlur(imgGray, imgGrayBlur, new Size(blurSize, blurSize), 0);
+
+            Mat diff = new Mat();
+            Cv2.Absdiff(refImage, imgMed, diff);
+
+            //Mat after = new Mat();
+            //Cv2.CvtColor(diff, after, ColorConversionCodes.BGR2GRAY);
+
+            //Cv2.MorphologyEx(imgGray, morpho, MorphTypes.TopHat, imgGray);
+
+
+
+
+            ////Do an invert binarize the image
+            Mat mask = new Mat();
+
+
+            ////Cv2.Subtract
+            if (mode == -1)
+                mask = diff;
+            else if (mode == 0)
+                Cv2.Threshold(diff, mask, thres, maxVal, ThresholdTypes.Binary);
+            else if (mode == 1)
+                Cv2.Threshold(diff, mask, thres, maxVal, ThresholdTypes.Triangle);
+            else if (mode == 2)
+                Cv2.Threshold(diff, mask, thres, maxVal, ThresholdTypes.BinaryInv);
+            else if (mode == 3)
+                Cv2.Threshold(diff, mask, thres, maxVal, ThresholdTypes.Trunc);
+            else if (mode == 4)
+                Cv2.Threshold(diff, mask, thres, maxVal, ThresholdTypes.Triangle);
+            else
+                Cv2.Threshold(diff, mask, thres, maxVal, ThresholdTypes.Tozero);
+
+
+            //////Extract edges
+            //Mat cannyEdges = new Mat();
+            //Cv2.Canny(mask, cannyEdges, canny1, canny2);
+
+            //Mat maskConverted = new Mat();
+            //Cv2.CvtColor(mask, maskConverted, ColorConversionCodes.BGR2YUV_YV12);
+            ////if (modeC)
+            ////{
+            //Point[][] countourPoints;
+            //HierarchyIndex[] hierarchy;
+            //Cv2.FindContours(maskConverted, out countourPoints, out hierarchy, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+            //if (countourPoints.Length > 0)
+            //{
+
+            //    Mat fillRec = new Mat(); ;
+            //    Cv2.FillConvexPoly(fillRec, countourPoints[0], Scalar.Blue);
+            //    // result, passing output texture as parameter allows to re-use it's buffer
+            //    // should output texture be null a new texture will be created
+            //}
+
+
+            output = Unity.MatToTexture(mask, output);
+            //}
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/Movetest.cs
@@ -0,0 +1,40 @@
+using Mirage;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movetest : NetworkBehaviour
+{
+
+    public float sbeve=1;
+
+    void Update()
+    {
+
+        if (IsLocalPlayer)
+        {
+            Move();
+
+        }
+        
+    }
+
+    void Move()
+    {
+        Vector2 moveDir=Vector2.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            moveDir.y = 1;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            moveDir.y = -1;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            moveDir.x = 1;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            moveDir.x = -1;
+
+        moveDir *= sbeve;
+
+        transform.position = new Vector3(transform.position.x + moveDir.x, transform.position.y + moveDir.y, transform.position.z);
+    }
+}
